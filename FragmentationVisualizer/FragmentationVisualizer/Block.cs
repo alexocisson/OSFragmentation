f6751fda40f61869717e9813f988e36a98d2c62c
alexis.portmann <alexis.portmann@he-arc.ch>
2021-03-29 17:38:25 +0200

    inti commit, proof of concept, debut affichage

---
+++ FragmentationVisualizer/FragmentationVisualizer/Block.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace FragmentationVisualizer
+{
+    class Block
+    {
+        private int index;
+        private Color color;
+
+        public void Draw(Canvas canvasToDraw, int pos)
+        {
+            int size = 20;
+            int margin = 5;
+            int columns = Convert.ToInt32(canvasToDraw.ActualWidth) / (size + margin);
+
+            Rectangle rectangle = new Rectangle
+            {
+                Height = size,
+                Width = size,
+            };
+            rectangle.Fill = Brushes.Green;
+            canvasToDraw.Children.Add(rectangle);
+
+            int row = pos / columns;
+            int column = pos - (row * columns);
+
+            Canvas.SetLeft(rectangle, column * (size + margin));
+            Canvas.SetTop(rectangle, row * (size + margin));
+        }
+    }
+}
---
+++ FragmentationVisualizer/FragmentationVisualizer/Memory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace FragmentationVisualizer
+{
+    class Memory
+    {
+        private const int N = 100;
+        private Block[] blocks;
+
+        public Memory()
+        {
+            blocks = new Block[N];
+            blocks[3] = new Block();
+            blocks[4] = new Block();
+            blocks[6] = new Block();
+            blocks[7] = new Block();
+        }
+
+        public void Draw(Canvas canvasToDraw)
+        {
+            for (int i = 0; i< N; i++)
+            {
+                if (blocks[i]!=null)
+                    blocks[i].Draw(canvasToDraw, i);
+            }
+        }
+    }
+}
